cae0f5dd2e642f9ec43704886485fc4c8ed975eb
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-09-29 23:18:44 +0530

    Added day_40

--- C_100days/day_40/Q79.c
+++ C_100days/day_40/Q79.c
@@ -11,3 +11,34 @@ Output 1:
 1 2 4 7 5 3 6 8 9
 
 */
+#include <stdio.h>
+int main()
+{
+    int r, c ;
+    printf("Enter number of rows and columns in matrix:\n");
+    scanf("%d %d", &r, &c);
+    int mat[r][c];
+
+    printf("Enter elements to matrix: \n");
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for(int k=0; k < r+c-1; k++ )
+    {
+        for(int i=0; i<r; i++)
+        {
+            for(int j=0; j<c; j++)
+            {
+                if(i+j == k)
+                printf("%d ", mat[i][j]);
+            }
+        }
+    }
+
+    return 0;
+}

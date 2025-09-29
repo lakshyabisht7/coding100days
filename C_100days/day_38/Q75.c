c0a07b57ae6fa6220d36df568528c9a44ad315db
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-09-29 19:34:21 +0530

    Added day_38

--- C_100days/day_38/Q75.c
+++ C_100days/day_38/Q75.c
@@ -13,3 +13,61 @@ Output 1:
 10 12
 
 */
+#include <stdio.h>
+int main()
+{
+    int r1, c1;
+    printf("Enter number of rows and columns in matrix 1:\n");
+    scanf("%d %d", &r1, &c1);
+    int mat1[r1][c1];
+
+    printf("Enter elements to matrix 1: \n");
+    for(int i=0; i<r1; i++)
+    {
+        for(int j=0; j<c1; j++)
+        {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    int r2, c2;
+    printf("Enter number of rows and columns in matrix 1:\n");
+    scanf("%d %d", &r2, &c2);
+    int mat2[r2][c2];
+
+    printf("Enter elements to matrix 1: \n");
+    for(int i=0; i<r2; i++)
+    {
+        for(int j=0; j<c2; j++)
+        {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    if(r1 != r2 || c1 != c2)
+    {
+        printf("Addition not possible");
+        return 1;
+    }
+    int sum_mat[r1][c1];
+
+    for(int i=0; i<r2; i++)
+    {
+        for(int j=0; j<c2; j++)
+        {
+            sum_mat[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+
+    for(int i=0; i<r2; i++)
+    {
+        for(int j=0; j<c2; j++)
+        {
+            printf("%d ", sum_mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
--- C_100days/day_38/Q76.c
+++ C_100days/day_38/Q76.c
@@ -16,3 +16,45 @@ Output 2:
 False
 
 */
+#include <stdio.h>
+int main()
+{
+    int r, c, f=1;
+    printf("Enter number of rows and columns in matrix:\n");
+    scanf("%d %d", &r, &c);
+    int mat[r][c];
+
+    if(r != c)
+    {
+        printf("Symmetric matrix = false");
+        return 0;
+    }
+
+    printf("Enter elements to matrix: \n");
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            if(mat[i][j] != mat[j][i])
+            {
+                f=0;
+                break;
+            }
+        }
+    }
+
+    if(f==1)
+    printf("Symmetric matrix = True");
+    else
+    printf("Symmetric matrix = False");
+    
+    return 0;
+}

7207b996363ba4bd94dfe0766c0983aa2930f02e
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-09-29 18:46:28 +0530

    Added day_37

--- C_100days/day_37/Q73.c
+++ C_100days/day_37/Q73.c
@@ -9,3 +9,32 @@ Output 1:
 6 15
 
 */
+#include <stdio.h>
+int main()
+{
+    int r, c, sum;
+    printf("Enter number of rows and columns in matrix:\n");
+    scanf("%d %d", &r, &c);
+    int mat[r][c];
+
+    printf("Enter elements to matrix: \n");
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for(int i=0; i<r; i++)
+    {
+        sum=0;
+        for(int j=0; j<c; j++)
+        {
+            sum += mat[i][j];
+        }
+        printf("Sum of elements of row %d = %d \n", i+1, sum);
+    }
+
+    return 0;
+}
--- C_100days/day_37/Q74.c
+++ C_100days/day_37/Q74.c
@@ -11,3 +11,40 @@ Output 1:
 3 6
 
 */
+#include <stdio.h>
+int main()
+{
+    int r, c;
+    printf("Enter number of rows and columns in matrix:\n");
+    scanf("%d %d", &r, &c);
+    int mat[r][c], transpose[c][r];
+
+    printf("Enter elements to matrix: \n");
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            transpose[j][i] = mat[i][j];
+        }
+    }
+
+    printf("Transposed matrix: \n");
+    for(int i=0; i<r; i++)
+    {
+        for(int j=0; j<c; j++)
+        {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

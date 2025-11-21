75e5f807105a09064851014756501b0810f34b4b
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-11-22 01:02:41 +0530

    Added day_83

--- C_100days/day_83/Q133.c
+++ C_100days/day_83/Q133.c
@@ -21,18 +21,18 @@ int main()
     scanf("%s", input);
 
     // Convert string to enum
-    if (strcmp(input, "JAN") == 0) m = JAN;
-    else if (strcmp(input, "FEB") == 0) m = FEB;
-    else if (strcmp(input, "MAR") == 0) m = MAR;
-    else if (strcmp(input, "APR") == 0) m = APR;
-    else if (strcmp(input, "MAY") == 0) m = MAY;
-    else if (strcmp(input, "JUN") == 0) m = JUN;
-    else if (strcmp(input, "JUL") == 0) m = JUL;
-    else if (strcmp(input, "AUG") == 0) m = AUG;
-    else if (strcmp(input, "SEP") == 0) m = SEP;
-    else if (strcmp(input, "OCT") == 0) m = OCT;
-    else if (strcmp(input, "NOV") == 0) m = NOV;
-    else m = DEC;
+    if(strcmp(input,"JAN") == 0) m=JAN;
+    else if(strcmp(input,"FEB") == 0) m=FEB;
+    else if(strcmp(input,"MAR") == 0) m=MAR;
+    else if(strcmp(input,"APR") == 0) m=APR;
+    else if(strcmp(input,"MAY") == 0) m=MAY;
+    else if(strcmp(input,"JUN") == 0) m=JUN;
+    else if(strcmp(input,"JUL") == 0) m=JUL;
+    else if(strcmp(input,"AUG") == 0) m=AUG;
+    else if(strcmp(input,"SEP") == 0) m=SEP;
+    else if(strcmp(input,"OCT") == 0) m=OCT;
+    else if(strcmp(input,"NOV") == 0) m=NOV;
+    else m=DEC;
 
     switch (m) 
     {

c06cb82c18f7b7236f7955d70b92c1ee2abc28d3
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-11-22 00:34:19 +0530

    Added day_94

---
+++ C_100days/day_94/Q144.c
@@ -0,0 +1,33 @@
+/*Q144: Write a function that accepts a structure as parameter and prints its members.
+
+Sample Test Cases:
+Input 1:
+Student: Neha 104 92
+Output 1:
+Name: Neha | Roll: 104 | Marks: 92
+
+*/
+#include <stdio.h>
+
+struct Student 
+{
+    char name[50];
+    int roll;
+    float marks;
+};
+
+void displayStudent(struct Student s) 
+{
+    printf("Name: %s | Roll: %d | Marks: %.0f\n", s.name, s.roll, s.marks);
+}
+
+int main() 
+{
+    struct Student s;
+
+    scanf("%s %d %f", s.name, &s.roll, &s.marks);
+
+    printStudent(s);
+
+    return 0;
+}

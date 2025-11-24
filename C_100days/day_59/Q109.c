a06f3abc3a26a3dc6a7a04e15651815bcb6862f5
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-11-24 23:04:46 +0530

    Added day_59

---
+++ C_100days/day_59/Q109.c
@@ -0,0 +1,51 @@
+/*Q109: Write a program to take an integer array arr and an integer k as inputs. Print the maximum sum of all the subarrays of size k.
+
+Sample Test Cases:
+Input 1:
+arr[100, 200, 300, 400] = , k = 2
+Output 1:
+700
+
+Input 2:
+arr[1, 4, 2, 10, 23, 3, 1, 0, 20] = , k = 4
+Output 2:
+39
+
+Input 3:
+arr[100, 200, 300, 400] = , k = 1
+Output 3:
+400
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int n,k;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter elements to array:\n");
+        scanf("%d",&arr[i]);
+    }
+
+    int currSum=0;
+    for (int i=0; i<k; i++)
+    currSum += arr[i];
+
+    int maxSum=currSum;
+
+    for (int i=k; i<n; i++) 
+    {
+        currSum = currSum-arr[i-k]+arr[i];  
+        if (currSum > maxSum)
+        maxSum=currSum;
+    }
+
+    printf("%d\n", maxSum);
+    return 0;
+}

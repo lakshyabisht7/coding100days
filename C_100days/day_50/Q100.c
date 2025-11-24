19b281e12fb48adbcff0d4eabf8c880b534cefe4
lakshyabisht7 <lakshya.bisht111@gmail.com>
2025-11-24 19:46:22 +0530

    Added day_50

---
+++ C_100days/day_50/Q100.c
@@ -0,0 +1,33 @@
+/*Q100: Print all sub-strings of a string.
+
+Sample Test Cases:
+Input 1:
+abc
+Output 1:
+a,ab,abc,b,bc,c
+
+*/
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+    char str[100];
+    scanf("%s",str);
+    int n=strlen(str);
+
+    for(int i=0; i<n; i++) 
+    {
+        for(int j=i; j<n; j++)
+        {
+            for(int k=i; k<=j; k++)
+            {
+                printf("%c", str[k]);
+            }
+
+            if(!(i == n-1 && j == n-1))
+            printf(",");
+        }
+    }
+
+    return 0;
+}
---
+++ C_100days/day_50/Q99.c
@@ -0,0 +1,20 @@
+/*Q99: Change the date format from dd/04/yyyy to dd-Apr-yyyy.
+
+Sample Test Cases:
+Input 1:
+15/04/2025
+Output 1:
+15-Apr-2025
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int day, month, year;
+    char *months[]={"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    scanf("%d/%d/%d", &day, &month, &year);
+    printf("%d-%s-%d\n", day, months[month], year);
+
+    return 0;
+}
